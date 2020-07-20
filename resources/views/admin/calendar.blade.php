3bf9a66a46848e9e29e4878150350993a9b8dfbc
gpflv <gpflv@ya.ru>
2020-07-20 17:16:07 +0300

    Календарь

--- resources/views/admin/calendar.blade.php
+++ resources/views/admin/calendar.blade.php
@@ -7,4 +7,20 @@
 {{--        {{$order->pavilion()->first()}}--}}
 {{--    @endforeach--}}
 
+<div id="my-calendar"></div>
+<script>
+    const myCalendar = new TavoCalendar('#my-calendar', {
+        locale: "ru",
+        date: "2020-12-21",
+        range_select: false,
+        highlight: ["2020-12-23"],
+        blacklist: ["2020-12-24","2020-12-25"],
+        highlight_sunday: false,
+        highlight_saturday: false,
+        multi_select: false,
+        future_select: true,
+        past_select: true
+    })
+
+</script>
 @endsection
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -9,10 +9,24 @@
             min-height: 70%;
         }
     </style>
+
+    {{--TavoCalendar start, сделать вывод не на всех страницах--}}
+    {{--moment.js для календаря--}}
+    <script src="https://cdn.jsdelivr.net/npm/moment@latest/min/moment-with-locales.min.js"></script>
+    <link rel="stylesheet" href="http://cretail.ru/tavo-calendar.css" />
+    <script src="http://cretail.ru/tavo-calendar.js"></script>
+    {{--TavoCalendar end--}}
+
+
+    <meta name="viewport" content="user-scalable=no, initial-scale=1.0, maximum-scale=1.0, width=device-width">
+
+
 </head>
 <body>
 <div class="col-lg-12 p-4 text-center">
-    Меню: <a href="admin/editPavilions">Беседки</a>, <a href="admin/editAdds">Допы</a>
+    <a href="/admin/calendar">Календарь</a> || <a href="/pavilions">Заказать беседку</a>
+    <br/>
+    <a href="/admin/editPavilions">Изм. беседки</a>, <a href="/admin/editAdds">Изм. допы</a>
 </div>
 <div class="container content">
     @yield('content')
